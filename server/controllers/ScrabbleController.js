868b7109dbce6007b3927e0c40f2bae0cabaf351
Arbie John <arbie1234@gmail.com>
2026-01-28 04:40:21 +0800

    Added word validation in the controller

--- server/controllers/ScrabbleController.js
+++ server/controllers/ScrabbleController.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const wordService = require('../services/WordService');
+const dictionaryApiService = require('../services/DictionaryApiService');
 
 class ScrabbleController {
     constructor() {
         this.router = express.Router();
+        this.dictionaryApiService = dictionaryApiService;
         this.setupRoutes();
     }
 
@@ -13,6 +15,13 @@ class ScrabbleController {
 
     getWordScore(req, res) {
         const { word } = req.query;
+
+        // validate word
+        const isValidWord = this.dictionaryApiService.validateWord(word);
+        if (!isValidWord) {
+            return res.status(400).json({ error: 'Invalid word' });
+        }
+
         const score = wordService.calculateScore(word);
         res.json({ word, score });
     }
