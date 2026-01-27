6e761199bc0218fa18b6603d0e00a4d0051f9662
Arbie John <arbie1234@gmail.com>
2026-01-28 03:57:36 +0800

    Updated scoring parameters and added letter scores configuration

--- server/services/WordService.js
+++ server/services/WordService.js
@@ -1,7 +1,19 @@
 class WordService {
+    constructor() {
+        this.letterScores = require('../config/letterScores.json');
+    }
+
+    // Handle score calculation
     calculateScore(word) {
-        // TODO: Implement word score logic
-        return 0;
+        let score = 0;
+        for (let i = 0; i < word.length; i++) {
+            const letter = word[i].toUpperCase(); // handle case sensitivity
+            const letterScore = this.letterScores.find((score) => score.letter === letter);
+            if (letterScore) {
+                score += letterScore.points;
+            }
+        }
+        return score;
     }
 }
 
